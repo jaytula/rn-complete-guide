c27a66888b991343937d47a460ca020d42ce3d0a
Jay T <jaytula@gmail.com>
2019-09-03 21:18:49 -0700

    Finish Lesson 23

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50}}>
-      <View>
-        <TextInput style={{borderColor: 'black', borderWidth: 1, padding: 10}} placeholder="Course Goal" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Course Goal" />
         <Button title="ADD" />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  }
+});
